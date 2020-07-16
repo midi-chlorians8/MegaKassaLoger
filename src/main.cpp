ae89fea0d45054008c7325ccba4d0761425c6b63
Ilya Romanovskiy <ilya.romanovskiy.2015@mail.ru>
2020-07-16 13:42:07 +0300

    buttonAdd

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include "GyverButton.h" 
 
+#define Line1_PIN1 2
+#define Line1_PIN2 3  
+
+GButton butt1(PIN2);
+GButton butt2(PIN3);
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200); while(!Serial){}
+  Serial3.begin(9600);  //while(!Serial3){}
+  Serial3.println("BT Started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    butt1.tick();   butt2.tick(); //Опрос обоих переключатилей линии 1
+      if (butt1.isSingle()) Serial.println("Single butt1");     // проверка на один клик
+      if (butt2.isSingle()) Serial.println("Single butt2");     // проверка на один клик
+
+
+
+      
+  int i;
+  i++;
+  i++;
+  i+=5;
+  Serial.println(i);
 }
