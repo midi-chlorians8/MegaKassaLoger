de849cf06b54648ce08d50fe36ddac8556ce4b1e
Ilya Romanovskiy <ilya.romanovskiy.2015@mail.ru>
2020-07-19 14:44:04 +0300

    add6

--- src/Definition.h
+++ src/Definition.h
@@ -1,13 +1,58 @@
 //Кнопки и счётчики срабатываний
+// Пакет для линии 1
 #define Line1_PIN1 2
 #define Line1_PIN2 3  
 uint16_t Line1_countToday=0; 
 String L1DataStr;
 
-long TimingButt1; // Переменная для записи времени когда нажата кнопка 1
-long TimingButt2; // Переменная для записи времени когда нажата кнопка 2
+unsigned long L1TimingButt1; // Переменная для записи времени когда нажата кнопка 1
+unsigned long L1TimingButt2; // Переменная для записи времени когда нажата кнопка 2
 
-bool OneZjmyakBut1=false; // static??
-bool OneZjmyakBut2=false; // static??
+bool L1OneZjmyakBut1=false; 
+bool L1OneZjmyakBut2=false; 
+unsigned long L1WaitObrabotkaButt; //Переменная хранит точку отсчёта от которой 300мсек отсчитываются при входе в анализ направления движения
+
+bool L1CanReadBool;
+// Пакет для линии 1
+
+// Пакет для линии 2
+#define Line2_PIN1 4
+#define Line2_PIN2 5  
+uint16_t Line2_countToday=0; 
+String L2DataStr;
+
+unsigned long L2TimingButt1; // Переменная для записи времени когда нажата кнопка 1
+unsigned long L2TimingButt2; // Переменная для записи времени когда нажата кнопка 2
+
+bool L2OneZjmyakBut1=false; 
+bool L2OneZjmyakBut2=false; 
+unsigned long L2WaitObrabotkaButt; //Переменная хранит точку отсчёта от которой 300мсек отсчитываются при входе в анализ направления движения
+
+bool L2CanReadBool;
+// Пакет для линии 2
 //Кнопки и счётчики срабатываний
 
+// RTC
+String MyDateTimeStr;
+#include <Wire.h> // must be included here so that Arduino library object file references work
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    //Serial.print(datestring);
+    MyDateTimeStr=datestring; // Забираю себе дату и время
+}
+// RTC
+
+void PrintDebug(); // Прототип
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,23 @@
 #include <Arduino.h>
 #include <Definition.h>
+//EEPROM
+#include <EEPROM.h>
 
-// RTC
-String MyDateTimeStr;
-#include <Wire.h> // must be included here so that Arduino library object file references work
-#include <RtcDS3231.h>
-RtcDS3231<TwoWire> Rtc(Wire);
-#define countof(a) (sizeof(a) / sizeof(a[0]))
-void printDateTime(const RtcDateTime& dt)
-{
-    char datestring[20];
-    snprintf_P(datestring, 
-            countof(datestring),
-            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-            dt.Month(),
-            dt.Day(),
-            dt.Year(),
-            dt.Hour(),
-            dt.Minute(),
-            dt.Second() );
-    //Serial.print(datestring);
-    MyDateTimeStr=datestring; // Забираю себе дату и время
-}
-// RTC
+struct MyObject {
+  byte NameLine;
+  int Line_countToday;
+  char Date_time[20];
+};
+int eeAddress = 0;   //Location we want the data to be put.
+//EEPROM
 void setup() {
-  Serial.begin(115200); while(!Serial){}
+  Serial.begin(19200); while(!Serial){}
   //Serial3.begin(9600);  //while(!Serial3){}  // Serial3.println("BT Started");
-
-  
 
   //Кнопки и счётчики срабатываний
   pinMode(Line1_PIN1,INPUT_PULLUP); pinMode(Line1_PIN2,INPUT_PULLUP);
-  Serial.print("TimingButt1:");Serial.print(TimingButt1);  Serial.print(" TimingButt2:");Serial.println(TimingButt2);
+  pinMode(Line2_PIN1,INPUT_PULLUP); pinMode(Line2_PIN2,INPUT_PULLUP);
+  Serial.print("TimingButt1:");Serial.print(L1TimingButt1);  Serial.print(" TimingButt2:");Serial.println(L1TimingButt2);
   Serial.println();Serial.println();Serial.println();
   //Кнопки и счётчики срабатываний
 
@@ -70,50 +56,54 @@ void setup() {
 }
 
 
-void ObrabotkaOneSide(String NameLine,int8_t pin,int8_t pin2,uint16_t& Line_countToday){ //Функция должна при нажатии последовательно кнопки 1, а затем кнопки 2 увеличить глобальный счётчик прохода в линии
-//void ObrabotkaOneSide(int8_t pin,int8_t pin2,uint16_t& Line_countToday){ //Функция должна при нажатии последовательно кнопки 1, а затем кнопки 2 увеличить глобальный счётчик прохода в линии
- 
- static unsigned long WaitObrabotkaButt; //Переменная для того чтоб убрать делей. В деле при обработке после нажатия обеих кнопок
- static bool CanReadBool = true;
+void ObrabotkaOneSide(String NameLine,int8_t pin,int8_t pin2,uint16_t& Line_countToday,bool& CanReadBool,unsigned long& TimingButtFirst,unsigned long& TimingButtSecond,bool& OneZjmyakButFirst,bool& OneZjmyakButSecond,unsigned long& WaitObrabotkaButt, String& DataStr){ //Функция должна при нажатии последовательно кнопки 1, а затем кнопки 2 увеличить глобальный счётчик прохода в линии
+
+ //static bool CanReadBool = true;
  
  //Ввод значений нажатия кнопки. Результат нажатия були OneZjmyakBut1 и OneZjmyakBut2 
  if(CanReadBool == true){ //Если разрешено читать кнопки
     if(digitalRead(pin) == 0){ // Если нажата кнопка 1
-      if(OneZjmyakBut1==false){
-      TimingButt1=millis(); // Присвоить значение таймера в переменную
-      //Serial.print("[pin]TimingButt1:");Serial.print(TimingButt1);Serial.print(" TimingButt2:");Serial.println(TimingButt2);
-      OneZjmyakBut1=true;
+      if(OneZjmyakButFirst==false){
+      TimingButtFirst=millis(); // Присвоить значение таймера в переменную
+      //Serial.print("[pin]TimingButt1:");Serial.print("");Serial.print(" TimingButt2:");Serial.println();
+      OneZjmyakButFirst=true;
       }
   }
     if(digitalRead(pin2) == 0){// Если нажата кнопка 2
-      if(OneZjmyakBut2==false){
-      TimingButt2=millis(); // Присвоить значение таймера в переменную
-      //Serial.print("[pin2]TimingButt1:");Serial.print(TimingButt1);Serial.print(" TimingButt2:");Serial.println(TimingButt2);
-      OneZjmyakBut2=true;
+      if(OneZjmyakButSecond==false){
+      TimingButtSecond=millis(); // Присвоить значение таймера в переменную
+      //Serial.print("[pin2]TimingButt1:");Serial.print("");Serial.print(" TimingButt2:");Serial.println();
+      OneZjmyakButSecond=true;
     }
   }
  }                      //Если разрешено читать кнопки
  //Ввод значений нажатия кнопки. Результат нажатия були OneZjmyakBut1 и OneZjmyakBut2 
  
  //Обработка значений. Прибавка счётчика нажатий
-   if(OneZjmyakBut1==true && OneZjmyakBut2 == true){ 
+   if(OneZjmyakButFirst==true && OneZjmyakButSecond == true){ 
    CanReadBool=false; // Запретить чтение кнопок
-      if( (TimingButt1-TimingButt2)<0  ){
+      //if( (TimingButt1-TimingButt2)<0  ){
+        if( TimingButtFirst < TimingButtSecond  ){
         Line_countToday++;
-        /*
-        //Serial.print(F("Proxod Dobavlen:")); Serial.print(F("TB1-TB2)<0 "));
-        Serial.print(NameLine); Serial.print(F("Line_countToday:")); 
-        Serial.print(Line_countToday); Serial.print(" ");
-        Serial.print(MyDateTimeStr); Serial.println();
-        */
-        L1DataStr=NameLine + Line_countToday + " " + MyDateTimeStr;
-        Serial.print("L1DataStr");Serial.println(L1DataStr);
+        
+        //Serial.print(F("Proxod Dobavlen:")); Serial.print(F("TB1-TB2)<0 "));       Serial.print(NameLine); Serial.print(F("Line_countToday:"));        Serial.print(Line_countToday); Serial.print(" ");        Serial.print(MyDateTimeStr); Serial.println();
+        
+        DataStr=NameLine + Line_countToday + " " + MyDateTimeStr;
+        Serial.print("DataStr");Serial.println(DataStr);
+        // Тут можно подцепится запись в EEPROM
+        MyObject customVar = {
+         NameLine,
+         65,
+        "07/19/2020 14:11:39"
+  };
+        OneZjmyakButFirst=false; OneZjmyakButSecond=false; // Перезаряд
       }
-      if( (TimingButt1-TimingButt2)>0  ){
-      Serial.print("Proxod v obratnyu storonu:"); Serial.println("TimingButt1-TimingButt2)>0");Serial.println();
+      //if( (TimingButt1-TimingButt2)>0  ){
+      if( TimingButtFirst > TimingButtSecond  ){
+      Serial.print("Proxod v obratnyu storonu:"); //Serial.println("TimingButt1-TimingButt2)>0");Serial.println();
       }
-      OneZjmyakBut1=false; OneZjmyakBut2=false; // Перезаряд
-      TimingButt1=0;       TimingButt2=0;       // Перезаряд
+      OneZjmyakButFirst=false; OneZjmyakButSecond=false; // Перезаряд
+      TimingButtFirst=0;       TimingButtSecond=0;       // Перезаряд
       WaitObrabotkaButt=millis(); // Присваиваим точку отсчёта переменной от которой будет отсчёт для активации буля возможности чтения кнопок
  //Обработка значений. Прибавка счётчика нажатий
 }
@@ -122,6 +112,7 @@ void ObrabotkaOneSide(String NameLine,int8_t pin,int8_t pin2,uint16_t& Line_coun
        WaitObrabotkaButt = millis(); //Serial.println ("10 seconds");
  }
 }
+
 unsigned long timingRefresh; // Переменная для хранения точки отсчета
 void loop() {
 if (millis() - timingRefresh > 1000){ // Вместо 10000 подставьте нужное вам значение паузы 
@@ -130,19 +121,27 @@ if (millis() - timingRefresh > 1000){ // Вместо 10000 подставьте
    timingRefresh = millis(); 
 }
 
-
- 
-  ObrabotkaOneSide("L1:",Line1_PIN1,Line1_PIN2,Line1_countToday); // захват прохода в одну сторону. Уведичение счётчика переданной линии
+  ObrabotkaOneSide("L1:",Line1_PIN1,Line1_PIN2,Line1_countToday,L1CanReadBool  ,L1TimingButt1,L1TimingButt2,  L1OneZjmyakBut1,L1OneZjmyakBut2 ,L1WaitObrabotkaButt, L1DataStr); // захват прохода в одну сторону. Уведичение счётчика переданной линии
+  ObrabotkaOneSide("L2:",Line2_PIN1,Line2_PIN2,Line2_countToday,L2CanReadBool  ,L2TimingButt1,L2TimingButt2,  L2OneZjmyakBut1,L2OneZjmyakBut2 ,L2WaitObrabotkaButt, L2DataStr); // захват прохода в одну сторону. Уведичение счётчика переданной линии
+  
   //ObrabotkaOneSide(Line1_PIN1,Line1_PIN2,Line1_countToday);
+  //PrintDebug();
 }
 
 
 
 
-
-
-
-
+void PrintDebug(){
+  static unsigned long timingPrint ;
+  if (millis() - timingPrint > 3000){ // Вместо 10000 подставьте нужное вам значение паузы 
+  Serial.println();
+  Serial.print("L1TimingButt1:");Serial.print(L1TimingButt1);  Serial.print(" L1TimingButt2:");Serial.print(L1TimingButt2); Serial.print(" L1OneZjmyakBut1:");Serial.print(L1OneZjmyakBut1); Serial.print(" L1OneZjmyakBut2:");Serial.println(L1OneZjmyakBut2);
+  Serial.print("L2TimingButt1:");Serial.print(L2TimingButt1);  Serial.print(" L2TimingButt2:");Serial.print(L2TimingButt2); Serial.print(" L2OneZjmyakBut1:");Serial.print(L2OneZjmyakBut1); Serial.print(" L2OneZjmyakBut2:");Serial.println(L2OneZjmyakBut2);
+  Serial.println();
+  timingPrint = millis(); 
+  }
+ 
+}
 
 
 
